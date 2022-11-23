8bd6b972e53c0191cb226352b107cfbf92c88eaf
MohdSabir786 <83216774+MohdSabir786@users.noreply.github.com>
2022-11-24 00:21:29 +0530

    Time: 1 ms (77.94%), Space: 42.5 MB (28.09%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,20 +14,30 @@
  * }
  */
 class Solution {
+    public boolean isMirror(TreeNode r1, TreeNode r2){
+        if(r1 == null && r2 == null) return true;
+        if(r1 == null || r2 == null) return false;
+        
+        if(r1.val != r2.val) return false;
+        return isMirror(r1.left, r2.right) && isMirror(r1.right, r2.left);
+    }
     public boolean isSymmetric(TreeNode root) {
-        Queue <TreeNode> q = new LinkedList<>();
-        q.offer(root.left);
-        q.offer(root.right);
-        while(!q.isEmpty()){
-            TreeNode n1 = q.poll();
-            TreeNode n2 = q.poll();
-            if(n1 == null && n2== null) continue;
-            if(n1 ==null ||  n2 == null || n1.val != n2.val) return false;
-            q.offer(n1.left);
-            q.offer(n2.right);
-            q.offer(n1.right);
-            q.offer(n2.left);
-        }
-        return true;
+//      This Solution have space O(n)   
+        
+        // Queue <TreeNode> q = new LinkedList<>();
+        // q.offer(root.left);
+        // q.offer(root.right);
+        // while(!q.isEmpty()){
+        //     TreeNode n1 = q.poll();
+        //     TreeNode n2 = q.poll();
+        //     if(n1 == null && n2== null) continue;
+        //     if(n1 ==null ||  n2 == null || n1.val != n2.val) return false;
+        //     q.offer(n1.left);
+        //     q.offer(n2.right);
+        //     q.offer(n1.right);
+        //     q.offer(n2.left);
+        // }
+        // return true;
+        return root == null || isMirror(root.left, root.right); 
     }
 }
